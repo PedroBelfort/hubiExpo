93fc9f153c0a5e092f7d06b29546a97b2f7c2f63
PedroBelfort <pedroamericocostabelfort@gmail.com>
2024-04-09 16:01:52 +0100

    feat: Ajuste de modal conforme prototipo

--- src/pages/Pedidos/index.js
+++ src/pages/Pedidos/index.js
@@ -1,11 +1,12 @@
-import React, {useState, useEffect} from "react";
-import { Modal, View , Button } from 'react-native';
-import { Banner, Text  } from 'react-native-paper';
-import {LinearGradient} from 'expo-linear-gradient';
+import React, { useState } from "react";
+import { Modal, View, Button, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { Text,Divider } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
 import Header from '../../components/Header';
 import TimeLineItem from '../../components/TimelineItem';
 import PedidoItem from '../../components/PedidoItem';
-import { useNavigation } from "@react-navigation/native"; 
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
 
 import {
   Container,
@@ -14,7 +15,15 @@ import {
   Title,
   ListHeader,
   ListPedidos,
+  ModalContainer,
+  ModalContent,
+  ModalText,
+  ModalButtonContainer,
+  ModalOption,
+  OptionCancelar
 } from './styles';
+
+const windowHeight = Dimensions.get('window').height;
 
 export default function Pedidos() {
 
@@ -28,22 +37,21 @@ export default function Pedidos() {
   };
 
   const closeModal = () => {
+    setPedidoDetails(null); 
     setVisibleModal(false);
   };
 
-
-  function navigatePedidoPage(item){
-    navigation.navigate('Pedido',{id:item})
+  function navigatePedidoPage(item) {
+    navigation.navigate('Pedido', { id: item });
   }
 
   return (
- 
-      <Container>
-    <LinearGradient
-      colors={['#009581', '#038F87','#2554BD']}
-      style={{ flex: 0.8 , margin:0}}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 1, y: 1 }}>
+    <Container>
+      <LinearGradient
+        colors={['#009581', '#038F87', '#2554BD']}
+        style={{ flex: 0.8, margin: 0 }}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}>
         <Header />
         <TimeLineContainer>
           <Title>Hoje</Title>
@@ -51,41 +59,49 @@ export default function Pedidos() {
             horizontal={true}
             showsHorizontalScrollIndicator={false}
             data={[1, 2, 3, 4, 5]}
-            renderItem={() => <TimeLineItem  />}
+            renderItem={() => <TimeLineItem />}
           />
         </TimeLineContainer>
-        </LinearGradient>
-        <ListHeader>
-          <Text variant="titleMedium">Pedidos</Text>
-        </ListHeader>
-        <ListPedidos
-          horizontal={false}
-          showsHorizontalScrollIndicator={false}
-          data={[1, 2, 3, 4, 5, 7, 8, 9, 10]}
-          renderItem={({ item }) => (
-            <PedidoItem data={item} navigatePage={() => navigatePedidoPage(item)} 
-            openModal={openModal} />
-          )}
-          keyExtractor={(item)=> String(item)}
-        />
+      </LinearGradient>
+      <ListHeader>
+        <Text variant="titleMedium">Pedidos</Text>
+      </ListHeader>
+      <ListPedidos
+        horizontal={false}
+        showsHorizontalScrollIndicator={false}
+        data={[1, 2, 3, 4, 5, 7, 8, 9, 10]}
+        renderItem={({ item }) => (
+          <PedidoItem
+            data={item}
+            navigatePage={() => navigatePedidoPage(item)}
+            openModal={openModal}
+          />
+        )}
+        keyExtractor={(item) => String(item)}
+      />
 
 <Modal
         visible={visibleModal}
         animationType="slide"
         onRequestClose={closeModal}
+        transparent={true}
       >
-        {/* Conteúdo do modal com os detalhes do pedido */}
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>{pedidoDetails?.nome}</Text>
-          <Text>{pedidoDetails?.local}</Text>
-          <Text>{pedidoDetails?.horario}</Text>
-          <Text>{pedidoDetails?.status}</Text>
-          {/* Botão para fechar o modal */}
-          <Button title="Fechar" onPress={closeModal} />
-        </View>
+        <ModalContainer>
+          <ModalContent>
+          <ModalButtonContainer onPress={closeModal}>
+            <Ionicons name="remove-outline" size={30} color={"black"}  />
+          </ModalButtonContainer>
+          <ModalOption>
+             <Text variant="titleMedium">Ver Serviço</Text>
+          </ModalOption>
+          <ModalOption>
+          <Ionicons name="trash" size={16} color={ "#CF5472"}  />
+             <OptionCancelar>Cancelar Serviço</OptionCancelar>
+          </ModalOption>
+          </ModalContent>
+        </ModalContainer>
       </Modal>
-  </Container>
-
-
+    </Container>
   );
 }
+
--- src/pages/Pedidos/styles.js
+++ src/pages/Pedidos/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
-
+const windowHeight = Dimensions.get('window').height;
 export const Container = styled.View`
 flex: 1;
 `;
@@ -35,3 +36,48 @@ height: 87px;
 `;
 
 
+export const ModalContainer = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const ModalContent = styled.View`
+  background-color: #FFF;
+  width: 100%;
+  max-height: ${windowHeight * 0.6}px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  padding-horizontal: 20px;
+  padding-top: 5px;
+`;
+
+export const ModalText = styled.Text`
+  margin-bottom: 10px;
+`;
+
+export const ModalButtonContainer = styled.TouchableOpacity`
+  width: 100%;
+  height: auto;
+  align-items:center;
+  vertical-align: auto;
+  padding: 2px;
+  border-bottom-width: 1px;
+  border-bottom-color: #D9D9D9; 
+`;
+
+export const ModalOption = styled.TouchableOpacity`
+height: 70px;
+flex-direction: row;
+align-items: center;
+padding-left: 14px;
+border-bottom-width: 1px;
+border-bottom-color: #E0E0E0;
+`;
+
+export const OptionCancelar = styled.Text`
+ color : #CF5472;
+ font-weight: 400;
+ padding-left: 14px;
+ font-size: 16px;
+`;
